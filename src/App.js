a2ba4b01eda89a782cf127308846e665ba6a38dc
Furkan Duran <f2durann@gmail.com>
2022-11-17 20:18:36 +0300

    17/11/2022

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import './App.css';
 //COMPONENTS
 //import Calback from './Components/Calback'
 //import ArrayFuntions from './Components/Giris/ArrayFunctions'
-import Appp from './Components/Giris/app'
+//import App from './Components/Giris/ArrayFunctions'
+import Appp from './Components/ComponentsKullanimi/app'
 function App() {
   return (
     <>
